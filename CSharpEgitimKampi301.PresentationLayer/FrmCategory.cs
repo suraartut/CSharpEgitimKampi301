491db510ca4285074ce5fb4a4b3bf1f950ff84d1
suraartut <suraartut@gmail.com>
2024-12-12 01:59:16 +0300

    20-Dependency Injection

--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.BusinessLayer.Concrete;
+using CSharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using CSharpEgitimKampi301.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,51 @@ namespace CSharpEgitimKampi301.PresentationLayer
 {
     public partial class FrmCategory : Form
     {
+        private readonly ICategoryService _categoryService;
+
         public FrmCategory()
         {
+            _categoryService = new CategoryManager(new EfCategoryDal());
             InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryValues = _categoryService.TGetAll();
+            dataGridView1.DataSource = categoryValues;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName = txtCatName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme Başarılı!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCatId.Text);
+            var deletedValue = _categoryService.TGetById(id);
+            _categoryService.TDelete(deletedValue);
+            MessageBox.Show("Silme Başarılı!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int updatedId = int.Parse(txtCatId.Text);
+            var updatedValue = _categoryService.TGetById(updatedId);
+            updatedValue.CategoryName = txtCatName.Text;
+            updatedValue.CategoryStatus = true;
+            _categoryService.TUpdate(updatedValue);
+            MessageBox.Show("Güncelleme Başarılı!");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCatId.Text);
+            dataGridView1.DataSource = _categoryService.TGetById(id);
         }
     }
 }
